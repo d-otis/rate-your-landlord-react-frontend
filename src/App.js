62d900b391e73f98fc01220749697f72eebc2fc4
d-otis <dan@dan-foley.com>
2020-10-21 14:55:40 -0400

    stub out NavBar links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/landlords">Landlords</Link>
+          </li>
+          <li>
+            <Link to="/properties">Properties</Link>
+          </li>
+        </ul>
+      </div>
+
+
+      <Switch>
+        <Route exact path="/">
+          Home!
+        </Route>
+        <Route exact path="/landlords">
+          Landlords
+        </Route>
+        <Route exact path="/properties">
+          Properties
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
