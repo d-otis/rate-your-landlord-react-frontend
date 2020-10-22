b666fe384ea3fa52d580b1117badcf3c6d33ab14
d-otis <dan@dan-foley.com>
2020-10-22 19:04:55 -0400

    add propertiesReducer file

---
+++ src/reducers/propertiesReducer.js
@@ -0,0 +1,15 @@
+export default function propertiesReducer(
+  state = [],
+  action
+) {
+  console.log('action is: ', action)
+  switch (action.type) {
+    case 'LOADING_LANDLORDS':
+      return state
+    case 'SET_LANDLORDS':
+    debugger
+      return action.payload 
+    default:
+      return state
+  }
+}
