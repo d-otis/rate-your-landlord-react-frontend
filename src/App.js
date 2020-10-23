fbfbef4b0b71da68a93ddc57fe54e305d5dabba1
d-otis <dan@dan-foley.com>
2020-10-23 12:44:01 -0400

    refactor App to utilize BrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 import NavBar from './components/NavBar'
+import { connect } from 'react-redux'
+import { fetchLandlords } from './actions/landlordActions'
+import { fetchProperties } from './actions/properties.actions'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Landlords from './containers/Landlords'
 
 function App() {
   return (
-    <NavBar />
-  );
+    <Router>
+      <NavBar />
+      <Route exact path='/landlords' component={Landlords} />
+    </Router>
+
+  )
+}
+
+}
+
+const mapStateToProps = state => {
+  return state
 }
 
 export default App;
