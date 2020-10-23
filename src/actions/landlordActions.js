40f3259d69d92a3edf280dafe37a2c9daa6d0982
d-otis <dan@dan-foley.com>
2020-10-23 10:18:16 -0400

    restructure POJO that landlordsAction uses for SET_LANDLORDS

--- src/actions/landlordActions.js
+++ src/actions/landlordActions.js
@@ -1,10 +1,9 @@
 export function fetchLandlords() {
   return (dispatch) => {
-    console.log(dispatch)
     dispatch({type: 'LOADING_LANDLORDS'})
     fetch('http://localhost:3000/api/v1/landlords')
       .then(res => res.json())
-      .then(json => dispatch({type: 'SET_LANDLORDS', landlords: json.data}))
+      .then(json => dispatch({type: 'SET_LANDLORDS', payload: json}))
       .catch(err => console.log(err))
   }
  }
