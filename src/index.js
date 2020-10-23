2f961265af3291a98752c0364882d3a2d70138c4
d-otis <dan@dan-foley.com>
2020-10-23 13:23:40 -0400

    remove obsolete code

--- src/index.js
+++ src/index.js
@@ -9,11 +9,6 @@ import thunk from 'redux-thunk'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// const store = createStore(
-//   rootReducer,
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// )
-
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
