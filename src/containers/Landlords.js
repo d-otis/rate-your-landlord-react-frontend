da0e94f431a8482b550c17aa711f9d83d82245be
d-otis <dan@dan-foley.com>
2020-10-23 10:24:04 -0400

    refactor deconstruction of incoming props

--- src/containers/Landlords.js
+++ src/containers/Landlords.js
@@ -12,13 +12,13 @@ class Landlords extends Component {
 
   render() {
 
-    // const landlords = this.props.data
+    const landlords = this.props.landlords
     // debugger
 
     return (
       <div>
         <h1>Landlords Index</h1>
-        {/* landlords.map(ll => <Landlord key={ll.id} {...ll} />) */}
+        {landlords.map(landlord => <Landlord key={landlord.id} {...landlord} />)}
       </div>
       
     )
