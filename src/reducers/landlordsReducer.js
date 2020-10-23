ee3456a1ce30a1bd37e67d20c23c0d75969cbfb6
d-otis <dan@dan-foley.com>
2020-10-23 10:21:53 -0400

    properly massage JSON data into returned state in SET_LANDLORDS action of landlordsReducer

--- src/reducers/landlordsReducer.js
+++ src/reducers/landlordsReducer.js
@@ -2,9 +2,21 @@ export default function landlordsReducer(
   state = [],
   action
 ) {
+  console.log('action is: ', action)
   switch (action.type) {
+    case 'LOADING_LANDLORDS':
+      return state
     case 'SET_LANDLORDS':
-      break
+      const landlords = action.payload.data
+
+      return landlords.map(landlord => {
+        return ({
+                  id: landlord.id, 
+                  name: landlord.attributes.name, 
+                  reviews: landlord.relationships.reviews.data, 
+                  properties: landlord.relationships.properties.data
+                })
+      })
     default:
       return state
   }
