32f9941a933f39573de5dd222c1570b7be66d64e
d-otis <dan@dan-foley.com>
2020-10-23 10:30:49 -0400

    add reviews and rating props to Landlord component

--- src/components/Landlord.js
+++ src/components/Landlord.js
@@ -2,15 +2,19 @@ import React from 'react'
 
 function Landlord(props) {
 
-  const { id, name, properties, reviews } = props
+  const { id, name, properties, reviews, rating } = props
 
   return (
     <div>
       <h2>{name}</h2>
-      <h3>Rating: XXX</h3>
+      <h3>Rating: {rating}</h3>
       <h3>Properties:</h3>
       <ol>
         {properties.map((property, idx) => <li key={property.id}>Property ID : {property.id}</li>)}
+      </ol>
+      <h3>Properties:</h3>
+      <ol>
+        {reviews.map((review, idx) => <li key={review.id}>Review ID : {review.id}</li>)}
       </ol>
       <hr />
     </div>
