7fc67aac784b731f6e78748a763fcdc3a38fc7bf
d-otis <dan@dan-foley.com>
2020-10-28 17:28:12 -0400

    add controlled edit form inside Landlord component

--- src/components/Landlord.js
+++ src/components/Landlord.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Landlord(props) {
 
+  const [ toggleEdit, setToggleEdit ] = useState(false)
+
   const { id, name, properties, reviews, rating } = props
+
+  const handleEditClick = () => {
+    setToggleEdit(!toggleEdit)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(`you've edited the ${name}`)
+    setToggleEdit(false)
+  }
+
+  const renderEditForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          value={name}
+        />
+        <input 
+          type="submit"
+          value="Save"
+        />
+      </form>
+
+    )
+  }
 
   return (
     <div key={id}>
       <h2>{name}</h2>
+      <button onClick={handleEditClick} >Edit Landlord</button>
+      <br />
+      {toggleEdit && renderEditForm()}
       <h3>Rating: {rating}</h3>
       <h3>Properties:</h3>
 
