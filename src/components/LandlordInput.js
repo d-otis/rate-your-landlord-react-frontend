50788e4ae926a97d1f315e20907be2786d58c87a
d-otis <dan@dan-foley.com>
2020-10-28 13:03:31 -0400

    add LandlordInput component

---
+++ src/components/LandlordInput.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { createLandlord } from '../actions/landlords.actions'
+import { connect } from 'react-redux'
+
+const LandlordInput = () => {
+
+  const [name, setName] = useState('')
+
+  const handleChange = e => {
+    setName(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log("here's the name", name)
+
+    setName('')
+  }
+
+  return (
+    <div>
+      <h1>LandlordInput</h1>
+      <form onSubmit={handleSubmit} >
+      <label htmlFor="landlord-name">Create a Landlord</label>
+      <br />
+      <input
+        type="text"
+        placeholder="Landlord Name"
+        id="landlord-name"
+        name="name"
+        value={name}
+        onChange={handleChange}
+      />
+      <br />
+      <input 
+        type="submit"
+      />
+      </form>
+    </div>
+
+  )
+}
+
+export default connect(null, { createLandlord })(LandlordInput)
