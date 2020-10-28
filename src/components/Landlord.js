6828e80371a4f4ca28ed32612f9a684044d3e8e2
d-otis <dan@dan-foley.com>
2020-10-28 14:17:20 -0400

    whitespace

--- src/components/Landlord.js
+++ src/components/Landlord.js
@@ -10,25 +10,21 @@ function Landlord(props) {
       <h3>Rating: {rating}</h3>
       <h3>Properties:</h3>
 
-      {
-        properties.length > 0 ? 
-
-        <ol>
-          {properties.map((property, idx) => <li key={property.id}>Property ID : {property.id}</li>)}
-        </ol>
-
-      : 
-
-        'doesnt have properties'
+      {properties.length > 0 
+        ? 
+          <ol>
+            {properties.map((property, idx) => <li key={property.id}>Property ID : {property.id}</li>)}
+          </ol>
+        : 'doesnt have properties'
       }
 
       <h3>Reviews:</h3>
-      {reviews.length > 0 ?
-        <ol>
-          {reviews.map((review, idx) => <li key={review.id}>Review ID : {review.id}</li>)}
-        </ol>
-      :
-        "doesn't have any reviews"}
+      {reviews.length > 0 
+        ?
+          <ol>
+            {reviews.map((review, idx) => <li key={review.id}>Review ID : {review.id}</li>)}
+          </ol>
+        : "doesn't have any reviews"}
      
       <hr />
     </div>
