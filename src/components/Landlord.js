337dde6bb7001df4755fda15951e8f2fa057069a
d-otis <dan@dan-foley.com>
2020-10-28 14:14:59 -0400

    add rendering conditional to Landlord component

--- src/components/Landlord.js
+++ src/components/Landlord.js
@@ -9,13 +9,27 @@ function Landlord(props) {
       <h2>{name}</h2>
       <h3>Rating: {rating}</h3>
       <h3>Properties:</h3>
-      <ol>
-        {properties.map((property, idx) => <li key={property.id}>Property ID : {property.id}</li>)}
-      </ol>
-      <h3>Properties:</h3>
-      <ol>
-        {reviews.map((review, idx) => <li key={review.id}>Review ID : {review.id}</li>)}
-      </ol>
+
+      {
+        properties.length > 0 ? 
+
+        <ol>
+          {properties.map((property, idx) => <li key={property.id}>Property ID : {property.id}</li>)}
+        </ol>
+
+      : 
+
+        'doesnt have properties'
+      }
+
+      <h3>Reviews:</h3>
+      {reviews.length > 0 ?
+        <ol>
+          {reviews.map((review, idx) => <li key={review.id}>Review ID : {review.id}</li>)}
+        </ol>
+      :
+        "doesn't have any reviews"}
+     
       <hr />
     </div>
   )
