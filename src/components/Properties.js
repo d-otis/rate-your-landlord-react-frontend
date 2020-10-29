f74cc65461a2e4e7073277516140626c229ef359
d-otis <dan@dan-foley.com>
2020-10-29 16:25:18 -0400

    define and implement generateProperties() in Properties component

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -2,8 +2,19 @@ import React from 'react'
 import Property from './Property'
 
 const Properties = props => {
+
+  const { properties } = props
+
+  const generateProperties = () => {
+    return properties.map(property => <Property key={property.id} {...property} />)
+  }
+
   return(
-    <h1>Properties Index</h1>
+    <div>
+      <h1>Properties Index</h1>
+      {generateProperties()}
+    </div>
+
   )
 }
 
