d7292811bded07cd0b23ff05478e6d4542fc91a9
d-otis <dan@dan-foley.com>
2020-10-29 11:58:48 -0400

    add LandlordEditForm

---
+++ src/components/LandlordEditForm.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const LandlordEditForm = (props) => {
+
+  const [ name, setName ] = useState(props.name)
+  const { setToggleEdit, updateLandlord } = props
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(`you've edited the ${name}`)
+    setToggleEdit(false)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text"
+        value={name}
+        onChange={e => setName(e.target.value)}
+        name="name"
+      />
+      <input 
+        type="submit"
+        value="Save"
+      />
+    </form>
+
+  )
+
+}
+
+export default LandlordEditForm
