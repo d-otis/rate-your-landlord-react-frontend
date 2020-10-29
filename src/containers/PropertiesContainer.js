2d694250adb5d40490df8823f6c89bf4cebc8acf
d-otis <dan@dan-foley.com>
2020-10-29 14:22:11 -0400

    boilerplate for PropertiesContainer

---
+++ src/containers/PropertiesContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class PropertiesContainer extends Component {
+  render() {
+    return (
+      <h1>PropertiesContainer</h1>
+    )
+  }
+}
+
+export default PropertiesContainer
