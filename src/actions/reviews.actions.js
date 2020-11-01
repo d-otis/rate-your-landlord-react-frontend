456e7d2232dd929437448059e97f06d1b7edbc42
d-otis <dan@dan-foley.com>
2020-11-01 12:04:43 -0500

    fetch using REVIEWS_PATH constant rather than duplicating the route everytime in the fetch()

--- src/actions/reviews.actions.js
+++ src/actions/reviews.actions.js
@@ -1,9 +1,11 @@
 import { BASE_URL } from '../utils/baseURL'
 
+const REVIEWS_PATH = BASE_URL + '/reviews'
+
 export function fetchReviews() {
   return (dispatch) => {
     dispatch({type: 'LOADING_REVIEWS'})
-    fetch(`${BASE_URL}/reviews`)
+    fetch(REVIEWS_PATH)
       .then(res => res.json())
       .then(json => dispatch({type: 'SET_REVIEWS', payload: json}))
       .catch(err => console.log(err))
