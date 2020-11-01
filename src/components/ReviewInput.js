bb51c928b5fc9bfea15095cc70316361f5441259
d-otis <dan@dan-foley.com>
2020-11-01 11:25:49 -0500

    flesh out ReviewInput component

---
+++ src/components/ReviewInput.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+const ReviewInput = ({ propertyId }) => {
+
+  const [ content, setContent ] = useState('')
+
+  const handleContentChange = e => {
+
+  }
+
+  return(
+    <div>
+      <form action="">
+        <textarea name="" id="" cols="30" rows="10" onChange={handleContentChange} value={content}></textarea>
+      <br />
+      <input type="submit" value="Save Review" />
+      </form>
+    </div>
+  )
+}
+
+export default ReviewInput
