4643fa82e2006405101dbe798061f2dbdee52c8f
d-otis <dan@dan-foley.com>
2020-11-01 11:51:49 -0500

    replace rating radio buttons with range slider

--- src/components/ReviewInput.js
+++ src/components/ReviewInput.js
@@ -24,23 +24,10 @@ const ReviewInput = ({ propertyId }) => {
       <form action="">
         <textarea name="" id="" cols="30" rows="5" onChange={handleContentChange} value={content}></textarea>
       <br />
-
-      <input type="radio" name="rating" value="5" id="five" />
-      <label htmlFor="five">5 Stars</label>
+      <label htmlFor="rating-range">Select a Rating</label>
       <br />
-      <input type="radio" name="rating" value="4" id="four" />
-      <label htmlFor="four">4 Stars</label>
+      <input type="range" min="1" max="5" id="raiting-range" onChange={handleRatingChange} /> {rating}
       <br />
-      <input type="radio" name="rating" value="3" id="three" />
-      <label htmlFor="three">3 Stars</label>
-      <br />
-      <input type="radio" name="rating" value="2" id="two" />
-      <label htmlFor="two">2 Stars</label>
-      <br />
-      <input type="radio" name="rating" value="1" id="one" />
-      <label htmlFor="one">1 Stars</label>
-      <br />
-
       <input type="submit" value="Save Review" />
       </form>
     </div>
