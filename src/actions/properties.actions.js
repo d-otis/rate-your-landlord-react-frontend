a64d974739ddde7da8d8ffb5286777a1c5fe0b65
d-otis <dan@dan-foley.com>
2020-11-01 12:00:10 -0500

    utilize BASE_URL in properties.actions

--- src/actions/properties.actions.js
+++ src/actions/properties.actions.js
@@ -3,7 +3,7 @@ import { BASE_URL } from '../utils/baseURL'
 export function fetchProperties() {
   return (dispatch) => {
     dispatch({type: 'LOADING_PROPERTIES'})
-    fetch('http://localhost:3000/api/v1/properties')
+    fetch(`${BASE_URL}/properties`)
       .then(res => res.json())
       .then(json => dispatch({type: 'SET_PROPERTIES', payload: json}))
       .catch(err => console.log(err))
