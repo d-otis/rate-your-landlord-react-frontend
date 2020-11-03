a33f7b1492f2068759fc39dfe6120c8e079a30d1
d-otis <dan@dan-foley.com>
2020-11-03 11:21:11 -0500

    remove fetchProperties() from import and as a prop

--- src/containers/PropertiesContainer.js
+++ src/containers/PropertiesContainer.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropertyInput from '../components/PropertyInput'
 import Properties from '../components/Properties'
-import { createProperty, fetchProperties } from '../actions/properties.actions'
+import { createProperty } from '../actions/properties.actions'
 import { createLandlord } from '../actions/landlords.actions'
 
 class PropertiesContainer extends Component {
   render() {
 
-    const { properties, createProperty, editProperty, deleteProperty, landlords, createLandlord, fetchProperties } = this.props
+    const { properties, createProperty, editProperty, deleteProperty, landlords, createLandlord } = this.props
 
     return (
       <div>
-        <PropertyInput createProperty={createProperty} landlords={landlords} createLandlord={createLandlord} fetchProperties={fetchProperties} />
+        <PropertyInput createProperty={createProperty} landlords={landlords} createLandlord={createLandlord} />
         <Properties properties={properties} editProperty={editProperty} deleteProperty={deleteProperty} landlords={landlords} />
       </div>
     )
@@ -26,4 +26,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, { createProperty, createLandlord, fetchProperties })(PropertiesContainer)
+export default connect(mapStateToProps, { createProperty, createLandlord })(PropertiesContainer)
