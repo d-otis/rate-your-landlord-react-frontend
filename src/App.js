bd8361abab0ac92403264a370da9555203f23873
d-otis <dan@dan-foley.com>
2020-11-03 14:37:24 -0500

    remove fetch calls from App root

--- src/App.js
+++ src/App.js
@@ -3,24 +3,11 @@ import React, { Component } from 'react';
 // import './App.css';
 import NavBar from './components/NavBar'
 import { connect } from 'react-redux'
-import { fetchLandlords } from './actions/landlords.actions'
-import { fetchProperties } from './actions/properties.actions'
-import { fetchReviews } from './actions/reviews.actions'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import LandlordsContainer from './containers/LandlordsContainer'
 import PropertiesContainer from './containers/PropertiesContainer'
 
 class App extends Component {
-
-  componentDidMount() {
-      this.fetchAll()
-    }
-
-  fetchAll = () => {
-    this.props.fetchLandlords()
-    this.props.fetchProperties()
-    this.props.fetchReviews()
-  }
 
     render() {
       const { landlords } = this.props
@@ -40,4 +27,4 @@ class App extends Component {
 
 const mapStateToProps = state => state
 
-export default connect(mapStateToProps, { fetchLandlords, fetchProperties, fetchReviews })(App);
+export default connect(mapStateToProps)(App);
