0a859db7b5e4f15ebee6e91f8023d9aa467fae3c
d-otis <dan@dan-foley.com>
2020-11-04 11:50:14 -0500

    remove unused components

--- src/components/ReviewsList.js
+++ src/components/ReviewsList.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import Review from './Review'
 import { connect } from 'react-redux'
 
