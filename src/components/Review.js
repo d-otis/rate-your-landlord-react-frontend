cad87287b861fe0207a80bac93176351d5a4600f
d-otis <dan@dan-foley.com>
2020-11-04 13:39:00 -0500

    styling

--- src/components/Review.js
+++ src/components/Review.js
@@ -8,11 +8,11 @@ const Review = ({ review }) => {
 
 
   return(
-      <Row className="bg-light rounded-lg pt-3 border mb-3"> {/*Review Outer Row*/}
+      <Row className="bg-light rounded-lg py-3 border mb-3"> {/*Review Outer Row*/}
         <Col> {/*Review Inner Column*/}
           <Container className="inner-review-container"> {/*Review Inner Container*/}
             <Row> {/*Review Inner Row*/}
-              <Col sm={2} className="review-rating text-center mt-3"> {/*Review Rating Column*/}
+              <Col sm={2} className="review-rating text-center my-3"> {/*Review Rating Column*/}
                 <h1><span className="bg-secondary text-white p-2 rounded-lg">{review?.rating}</span></h1>
               </Col> {/*Review Rating Column*/}
               <Col sm={10} className="review-content"> {/*Review Content Column*/}
