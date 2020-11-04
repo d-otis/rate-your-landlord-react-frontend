22e79ac4f43897d0df236797a62750e45e5c3a3d
d-otis <dan@dan-foley.com>
2020-11-04 17:40:36 -0500

    styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,7 +10,7 @@ class NavBar extends Component {
 
   render() {
     return (
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="light" expand="md" className="mb-3">
         <Navbar.Brand href="/">Rate Your Landlord</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
