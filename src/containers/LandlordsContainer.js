1746e4d34e65e3284df0554bee6a38a486bbb380
d-otis <dan@dan-foley.com>
2020-11-04 17:49:54 -0500

    import useState() and set up toggle for display of LandlordInput

--- src/containers/LandlordsContainer.js
+++ src/containers/LandlordsContainer.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import Landlords from '../components/Landlords'
 import LandlordInput from '../components/LandlordInput'
 import { connect } from 'react-redux'
 import Container from 'react-bootstrap/Container'
 
 const LandlordsContainer = ({ landlords }) => {
+
+  const [ showLandlordInput, setShowLandlordInput ] = useState(false)
+
+  const toggleLandlordInput = () => {
+    setShowLandlordInput(!showLandlordInput)
+  }
   
   return (
     <Container>
