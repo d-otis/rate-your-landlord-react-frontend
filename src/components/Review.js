185602a6f736e0313831c8a21de84b9773be92b1
d-otis <dan@dan-foley.com>
2020-11-04 10:59:41 -0500

    add review content to Review component

--- src/components/Review.js
+++ src/components/Review.js
@@ -15,6 +15,9 @@ const Review = ({ review }) => {
               <Col sm={2} className="review-rating text-center mt-3"> {/*Review Rating Column*/}
                 <h1><span className="bg-secondary text-white p-2 rounded-lg">{review?.rating}</span></h1>
               </Col> {/*Review Rating Column*/}
+              <Col sm={10} className="review-content"> {/*Review Content Column*/}
+                <p>{review?.content}</p>
+              </Col> {/*Review Content Column*/}
             </Row> {/*Review Inner Row*/}
           </Container> {/*Review Inner Container*/}
         </Col> {/*Review Inner Column*/}
