d3429a3614a25581fc98a1daf31c0384cff6b9e5
d-otis <dan@dan-foley.com>
2020-11-04 17:39:51 -0500

    refactor LandlordsContainer to make it a functional component

--- src/containers/LandlordsContainer.js
+++ src/containers/LandlordsContainer.js
@@ -4,19 +4,14 @@ import LandlordInput from '../components/LandlordInput'
 import { connect } from 'react-redux'
 import Container from 'react-bootstrap/Container'
 
-class LandlordsContainer extends Component {
-
-  render() {
-
-    const { landlords } = this.props
-    
-    return (
-      <Container>
-        <LandlordInput />
-        <Landlords landlords={landlords} />
-      </Container>
-    )
-  }
+const LandlordsContainer = ({ landlords }) => {
+  
+  return (
+    <Container>
+      <LandlordInput />
+      <Landlords landlords={landlords} />
+    </Container>
+  )
 }
 
 const mapStateToProps = (state) => {
