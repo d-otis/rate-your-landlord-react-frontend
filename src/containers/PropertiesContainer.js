54f93e3e521142a311b58817239f798fcb00794f
d-otis <dan@dan-foley.com>
2020-11-05 18:41:53 -0500

    remove unused components from react-router-dom import

--- src/containers/PropertiesContainer.js
+++ src/containers/PropertiesContainer.js
@@ -7,7 +7,7 @@ import { fetchReviews } from '../actions/reviews.actions'
 import { createLandlord } from '../actions/landlords.actions'
 import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
-import { useRouteMatch, Switch, Route, Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import Property from '../components/properties/Property'
 
 const PropertiesContainer = ({ properties, createProperty, editProperty, deleteProperty, landlords, createLandlord }) => {
