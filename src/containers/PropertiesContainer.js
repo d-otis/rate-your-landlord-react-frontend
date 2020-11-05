741b187f63c975ca2993fbca67109f33223a62e1
d-otis <dan@dan-foley.com>
2020-11-05 18:41:19 -0500

    import useState()

--- src/containers/PropertiesContainer.js
+++ src/containers/PropertiesContainer.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import PropertyInput from '../components/properties/PropertyInput'
 import PropertiesList from '../components/properties/PropertiesList'
