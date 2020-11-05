aca394f81948f0328c5fe51387602fd540cb0fdc
d-otis <dan@dan-foley.com>
2020-11-05 17:22:07 -0500

    incorporate bootstrap components

--- src/components/properties/PropertiesList.js
+++ src/components/properties/PropertiesList.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import Property from './Property'
 import { useRouteMatch, Switch, Route, Link } from 'react-router-dom'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 const PropertiesList = ({ properties, landlords }) => {
 
   let { path } = useRouteMatch()
 
   const generatePropertiesList = () => {
-    return Object.keys(properties).map(id=> <li key={id}><Link to={`/properties/${id}`}> {properties[id].address} </Link></li>)
+    return Object.keys(properties).map(id=> 
+      <Card key={id} className="my-4">
+      <Card.Header as="h3"> {properties[id].address} </Card.Header>
+      <Card.Body>
+        <Card.Title>Landlord: Mr Bojangles</Card.Title>
+        <Card.Text>This property has {properties[id].reviews.length} reviews.</Card.Text>
+        <Link to={`/properties/${id}`}><Button variant="secondary">Read the reviews</Button></Link>
+      </Card.Body>
+        
+      </Card>
+    )
   }
 
   return(
