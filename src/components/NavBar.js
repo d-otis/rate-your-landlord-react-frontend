21e0106b02a6cd02bfa1a7e1ab709cf04dbc8253
d-otis <dan@dan-foley.com>
2020-11-06 10:45:59 -0500

    refactor NavBar component to include reactstrap version of NavLink so that Link is used and clicking doesn't trigger a refresh of everything

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import { NavLink } from 'reactstrap'
+import { Link } from 'react-router-dom'
 
-class NavBar extends Component {
+const NavBar = () => {
 
-  render() {
-    return (
-      <Navbar bg="light" expand="md" className="mb-3">
-        <Navbar.Brand href="/">Rate Your Landlord</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/properties">Properties</Nav.Link>
-            <Nav.Link href="/landlords">Landlords</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Form inline>
-            <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Navbar>
-    )
-  }
+  return (
+    <Navbar bg="light" expand="md" className="mb-3">
+      <Navbar.Brand href="/">Rate Your Landlord</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <NavLink tag={Link} to="/properties">Properties</NavLink>
+          <NavLink tag={Link} to="/landlords">Landlords</NavLink>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Form inline>
+          <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
+  )
 }
 
-export default NavBar;
+export default NavBar
