8d06149970077fd977330bd9d0d8fde8bbe85701
d-otis <dan@dan-foley.com>
2020-11-09 17:42:55 -0500

    add review button and whitespace edits

--- src/components/properties/PropertiesList.js
+++ src/components/properties/PropertiesList.js
@@ -5,28 +5,31 @@ import Button from 'react-bootstrap/Button'
 
 const PropertiesList = ({ properties, landlords }) => {
 
-
+  const reviewButton = property => {
+    const buttonText = property.reviews.length === 0 ? 'Leave a Review' : "Read the Reviews"
+    return <Link to={`/properties/${property.id}`}><Button variant="secondary">{buttonText}</Button></Link>
+  }
 
   const generatePropertiesList = () => {
-    return Object.keys(properties).map(id=> 
-      <Card key={id} className="my-4">
-      <Card.Header as="h3"> {properties[id].address} </Card.Header>
-      <Card.Body>
-        <Card.Title>Landlord: Mr Bojangles</Card.Title>
-        <Card.Text>This property has {properties[id].reviews.length} reviews.</Card.Text>
-        <Link to={`/properties/${id}`}><Button variant="secondary">Read the reviews</Button></Link>
-      </Card.Body>
-        
-      </Card>
-    )
+    return Object.keys(properties).map(id=> {
+      const landlord = landlords[properties[id].landlordId]
+      console.log(landlord)
+      return (
+        <Card key={id} className="my-4">
+          <Card.Header as="h3"> {properties[id].address} </Card.Header>
+          <Card.Body>
+            <Card.Title>Landlord: {landlord?.name}</Card.Title>
+            <Card.Text>This property has {properties[id].reviews.length} reviews.</Card.Text>
+            {reviewButton(properties[id])}
+          </Card.Body>
+        </Card>
+      )
+    })
   }
 
   return(
     <div>
       {generatePropertiesList()}
-
-
-
     </div>
   )
 }
