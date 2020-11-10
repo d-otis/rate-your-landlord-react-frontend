ac7b3a1fe4d5bc25a8d451521458c2949172a467
d-otis <dan@dan-foley.com>
2020-11-10 10:21:04 -0500

    fix issue: only respond to ADD_LANDLORD in properties.reducer when landlord has included items aka not when a Landlord is created without any of their own resources

--- src/reducers/properties.reducer.js
+++ src/reducers/properties.reducer.js
@@ -67,22 +67,27 @@ export default function propertiesReducer(
       // Parse through returned JSON and 
       // add property data from returned new Landlord
       // ass to properties!
-      propertyJSON = action.payload.included[0]
+      if (action.payload.included.length !== 0) {
+        propertyJSON = action.payload.included[0]
 
-      newProperty = {
-        id: propertyJSON.id,
-        address: propertyJSON.attributes.address,
-        reviews: [],
-        landlordId: propertyJSON.attributes.landlord_id,
-        imageUrl: propertyJSON.attributes.image_url,
-        hasImage: propertyJSON.attributes.has_image,
-        rating: formatRating(propertyJSON.attributes.rating)
+        newProperty = {
+          id: propertyJSON.id,
+          address: propertyJSON.attributes.address,
+          reviews: [],
+          landlordId: propertyJSON.attributes.landlord_id,
+          imageUrl: propertyJSON.attributes.image_url,
+          hasImage: propertyJSON.attributes.has_image,
+          rating: formatRating(propertyJSON.attributes.rating)
+        }
+
+        stateCopy = Object.assign({}, state)
+        stateCopy[newProperty.id] = newProperty
+
+        return stateCopy
+      } else {
+        return state
       }
 
-      stateCopy = Object.assign({}, state)
-      stateCopy[newProperty.id] = newProperty
-
-      return stateCopy
 
     case 'DELETE_LANDLORD':
       let landlordId = action.payload.data.id
