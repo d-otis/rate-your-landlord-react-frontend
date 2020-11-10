3d5d29122b4beacc81db862db13abbf9abace94b
d-otis <dan@dan-foley.com>
2020-11-10 11:36:45 -0500

    remove link from Navbar.Brand

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,11 +10,12 @@ import { Link } from 'react-router-dom'
 const NavBar = () => {
 
   return (
-    <Navbar bg="light" expand="md" className="mb-3">
-      <Navbar.Brand href="/">Rate Your Landlord</Navbar.Brand>
+    <Navbar bg="light" expand="md" className="mb-4">
+      <Navbar.Brand>Rate Your Landlord</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
+          <NavLink tag={Link} to="/">Home</NavLink>
           <NavLink tag={Link} to="/properties">Properties</NavLink>
           <NavLink tag={Link} to="/landlords">Landlords</NavLink>
           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
