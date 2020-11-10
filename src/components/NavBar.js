423c12ff65b819e69cc73c576a54050f029ceac3
d-otis <dan@dan-foley.com>
2020-11-10 12:17:46 -0500

    remove unused code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
 import { NavLink } from 'reactstrap'
 import { Link } from 'react-router-dom'
 
@@ -18,18 +15,7 @@ const NavBar = () => {
           <NavLink tag={Link} to="/">Home</NavLink>
           <NavLink tag={Link} to="/properties">Properties</NavLink>
           <NavLink tag={Link} to="/landlords">Landlords</NavLink>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
         </Nav>
-        <Form inline>
-          <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
       </Navbar.Collapse>
     </Navbar>
   )
