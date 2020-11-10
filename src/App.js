c3f813394e6b5927181f8f6d1c4733481b7bff15
d-otis <dan@dan-foley.com>
2020-11-10 12:49:57 -0500

    import and implement ReviewsContainer in main App routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { connect } from 'react-redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import LandlordsContainer from './containers/LandlordsContainer'
 import PropertiesContainer from './containers/PropertiesContainer'
+import ReviewsContainer from './containers/ReviewsContainer'
 import { fetchLandlords } from './actions/landlords.actions'
 import { fetchProperties } from './actions/properties.actions'
 import { fetchReviews } from './actions/reviews.actions'
@@ -26,6 +27,7 @@ class App extends Component {
           <Route exact path="/" render={routerProps => <Home {...routerProps} />} />
           <Route path='/landlords' render={routerProps => <LandlordsContainer {...routerProps} />} />
           <Route path='/properties' render={routerProps => <PropertiesContainer {...routerProps} />} />
+          <Route path='/reviews' render={routerProps => <ReviewsContainer {...routerProps} />} />
       </Router>
     )
   }
