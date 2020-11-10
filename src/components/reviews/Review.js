00b34298d713a9e9389ad25b90f8573aed015f35
d-otis <dan@dan-foley.com>
2020-11-10 12:51:55 -0500

    add and use Link component in Review

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { Link}  from 'react-router-dom'
 
 const Review = ({ review }) => {
 
@@ -17,6 +18,7 @@ const Review = ({ review }) => {
               </Col> {/*Review Rating Column*/}
               <Col sm={10} className="review-content"> {/*Review Content Column*/}
                 <p>{review?.content}</p>
+                {isPropertyShow || <Link to={`/properties/${review.propertyId}`}>{review.address}</Link>}
               </Col> {/*Review Content Column*/}
             </Row> {/*Review Inner Row*/}
           </Container> {/*Review Inner Container*/}
