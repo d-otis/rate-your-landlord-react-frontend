0acdcfd1d65bf749e1b9786e7260446fc265577c
d-otis <dan@dan-foley.com>
2020-11-11 15:20:16 -0500

    create SortingComponent

---
+++ src/components/reviews/SortingComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Col from 'react-bootstrap/Col'
+
+const SortingComponent = () => {
+  return (
+    <Col>
+      <h1>Sorting Component</h1>
+    </Col>
+  )
+}
+
+export default SortingComponent
