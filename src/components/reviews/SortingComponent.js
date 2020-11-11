5178cc8d9c5dad55f15120ead153a10c742525ae
d-otis <dan@dan-foley.com>
2020-11-11 16:34:06 -0500

    flesh out sorting component

--- src/components/reviews/SortingComponent.js
+++ src/components/reviews/SortingComponent.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
 
-const SortingComponent = () => {
+const SortingComponent = ({ sortBy, setSortBy }) => {
+
+  const handleChange = e => {
+    setSortBy(e.target.value)
+  }
+
   return (
     <Col>
-      <h1>Sorting Component</h1>
+      <Form>
+        <Form.Label>Sort by:</Form.Label>
+        <Form.Group>
+          <fieldset>
+            <div key='inline-radio'>
+              <Form.Check type='radio' name="group" inline label="Chronological" id="none" onChange={handleChange} value="none"/>
+              <Form.Check type='radio' name="group" inline label="Descending" id="desc" onChange={handleChange} value="desc" />
+              <Form.Check type='radio' name="group" inline label="Ascending" id="asc" onChange={handleChange} value="asc" />
+            </div>
+          </fieldset>
+        </Form.Group>
+
+      </Form>
     </Col>
   )
 }
