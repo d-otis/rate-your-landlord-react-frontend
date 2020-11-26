1a72c6212abad70e4177db65369bb7e420010377
d-otis <dan@dan-foley.com>
2020-11-26 15:33:32 -0500

    update BASE_URL

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,1 +1,2 @@
-export const BASE_URL = process.env.BASE_URL || 'http://localhost:3000/api/v1'
+// export const BASE_URL = process.env.BASE_URL || 'http://localhost:3000/api/v1'
+export const BASE_URL = 'https://ryl-be.herokuapp.com/api/v1'
