c120b3ba21bfa9bd980c606cf1d40174adc45e9e
d-otis <dan@dan-foley.com>
2020-11-26 16:03:05 -0500

    use explicit api url instead of variable-derived

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = process.env.BASE_URL || 'http://localhost:3000/api/v1'
-// export const BASE_URL = 'https://ryl-be.herokuapp.com/api/v1'
-export const { BASE_URL } = process.env
+export const BASE_URL = 'https://ryl-be.herokuapp.com/api/v1'
+// export const { BASE_URL } = process.env
