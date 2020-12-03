23d3ae9fd3ac88dd6361dbaf0d80b000088860fb
d-otis <dan@dan-foley.com>
2020-12-03 12:40:07 -0500

    import Spinner and connect() to Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,8 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import Spinner from './Spinner'
 
 const Home = () => {
 
