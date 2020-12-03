2dfd11c54de2feb4a1cdc57e755898cb98d44cf6
d-otis <dan@dan-foley.com>
2020-12-03 13:16:59 -0500

    import and utilize bootstrap layout components

--- src/components/spinner/Spinner.jsx
+++ src/components/spinner/Spinner.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import "./Spinner.scss"
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const Spinner = () => {
   return(
-    "Spinner"
+    <Container className="text-center">
+      <Row>
+        <Col>
+          <img src="/spinner.gif"/>
+          <h3>Loading</h3>
+        </Col>
+      </Row>
+
+    </Container>
   )
 }
 
